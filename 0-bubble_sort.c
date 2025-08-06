085514c57c4a5fca741504a8ab6ebc5afbd3db59
Le-Meur-Mai <mai.lm@outlook.fr>
2025-08-06 14:44:51 +0200

    Modification of size and the 0-O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t size_array = size;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size < 1)
 		return;
 	while (size_array > 0)
 	{
